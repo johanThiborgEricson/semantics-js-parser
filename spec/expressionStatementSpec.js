80e13d7000d35b23ce9ecd52579c44350b28a0ff
Johan Thiborg-Ericson <johan.thiborg.ericson@gmail.com>
2017-08-03 19:19:52 +0200

    I will drop the "Spec" part of the filenames of specifications.

--- spec/expressionStatementSpec.js
+++
@@ -1,9 +0,0 @@
-describe("An expression statement", function() {
-  it("can set the value of a declared variable", function() {
-    var global = {};
-    var js = new JavaScriptParser(global);
-    
-    js.program("var a;a=1");
-    expect(global.a).toBe(1);
-  });
-});
