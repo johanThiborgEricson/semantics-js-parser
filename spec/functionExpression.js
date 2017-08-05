665e5ca15930a951a8a9eddd4d66f42d166006a5
Johan Thiborg-Ericson <johan.thiborg.ericson@gmail.com>
2017-08-05 21:22:40 +0200

    I added "line breaks" for better readability.

--- spec/functionExpression.js
+++ spec/functionExpression.js
@@ -3,25 +3,40 @@ describe("A function expression", function() {
   var js = new JavaScriptParser();
     
   it("returns a function", function() {
-    js.program("var f=function(){};");
+    js.program(
+      "var f=function(){};");
     
     expect(js.global.f).toEqual(jasmine.any(Function));
   });
   
   it("can be called", function() {
-    js.program("var a;var f=function(){a=1;};f();");
+    js.program(
+      "var a;" + 
+      "var f=function(){" + 
+        "a=1;" + 
+      "};" + 
+      "f();");
     
     expect(js.global.a).toBe(1);
   });
   
   it("creates a execution context", function() {
-    js.program("var a=0;var f=function(){var a=1;};f();");
+    js.program(
+      "var a=0;" + 
+      "var f=function(){" + 
+        "var a=1;" + 
+      "};" + 
+      "f();");
     
     expect(js.global.a).toBe(0);
   });
   
   it("can return a value", function() {
-    js.program("var f=function(){return 1;};var a=f();");
+    js.program(
+      "var f=function(){" + 
+        "return 1;" + 
+      "};" + 
+      "var a=f();");
     
     expect(js.global.a).toBe(1);
   });
